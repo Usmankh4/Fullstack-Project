1cf4a7084366d6c1037f02db4e434f6efece86d2
Usmankh4 <usmankhanx3@hotmail.com>
2023-11-30 21:26:49 -0500

    Code repair last page

--- src/app/repair/[model]/[repair]/page.js
+++ src/app/repair/[model]/[repair]/page.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import repairServices from "../../../repairBrand.json";
 const RepairLastPage = ({params}) => {
-   
+
+    const repairBrand = repairServices.filter(p => p.name === params.model);
+    console.log(params);
+
     return (
         <div>
             <Header />
 
-            {/* Main content */}
             <div>
-                {/* Your page content goes here */}
                 <h1>{params.model}</h1>
             </div>
 
-           
+
 
             <Footer />
         </div>
