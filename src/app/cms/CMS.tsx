b325a2bb03018fafe6029feaad0b7600a05b27f4
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2023-12-16 21:23:59 -0500

    added neew product collection

--- src/app/cms/CMS.tsx
+++ src/app/cms/CMS.tsx
@@ -20,164 +20,111 @@ const locales = {
 };
 
 type Product = {
-    name: string;
-    price: number;
-    status: string;
-    published: boolean;
-    related_products: EntityReference[];
-    main_image: string;
-    tags: string[];
-    description: string;
-    categories: string[];
-    publisher: {
-        name: string;
-        external_id: string;
-    },
-    expires_on: Date
-}
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  hasSizes: boolean;
+  quantity: number; // For products without sizes
+  sizes?: { [size: string]: number }; // For products with sizes
+  tags: string[];
+};
 
-const localeCollection = buildCollection({
-    path: "locale",
-    customId: locales,
-    name: "Locales",
-    singularName: "Locales",
-    properties: {
-        name: {
-            name: "Title",
-            validation: { required: true },
-            dataType: "string"
-        },
-        selectable: {
-            name: "Selectable",
-            description: "Is this locale selectable",
-            dataType: "boolean"
-        }
-    }
-});
-
-const productsCollection = buildCollection<Product>({
+const ProductsCollection = buildCollection<Product>({
     name: "Products",
     singularName: "Product",
-    path: "products",
-    permissions: ({ authController }) => ({
-        edit: true,
-        create: true,
-        // we have created the roles object in the navigation builder
-        delete: false
-    }),
-    subcollections: [
-        localeCollection
-    ],
+    path: "Products",
+    group: "Products page",
     properties: {
-        name: {
-            name: "Name",
-            validation: { required: true },
-            dataType: "string"
+      name: buildProperty({
+        dataType: "string",
+        name: "Name",
+        description: "The name of the product.",
+        validation: { required: true },
+      }),
+      price: buildProperty({
+        dataType: "number",
+        name: "Price",
+        description: "The price of the product in your preferred currency.",
+        validation: { required: true, min: 0 },
+      }),
+      description: buildProperty({
+        dataType: "string",
+        name: "Description",
+        description: "A detailed description of the product.",
+        validation: { required: true },
+      }),
+      image: buildProperty({
+        dataType: "string",
+        title: "Image",
+        description: "Upload an image for the product.",
+        storage: {
+          storagePath: "images/products",
+          acceptedFiles: ["image/png", "image/jpg", "image/jpeg"],
+          maxSize: 1920 * 1080,
+          metadata: {
+            cacheControl: "max-age=1000000",
+          },
         },
-        price: {
-            name: "Price",
-            validation: {
-                required: true,
-                requiredMessage: "You must set a price between 0 and 1000",
-                min: 0,
-                max: 1000
-            },
-            description: "Price with range validation",
-            dataType: "number"
+      }),
+      tags: buildProperty({
+        dataType: "array",
+        name: "Tags",
+        description:
+          "Tags for categorizing the product. Can include multiple tags.",
+        of: {
+          dataType: "string",
         },
-        status: {
-            name: "Status",
-            validation: { required: true },
-            dataType: "string",
-            description: "Should this product be visible in the website",
-            longDescription: "Example of a long description hidden under a tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis bibendum turpis. Sed scelerisque ligula nec nisi pellentesque, eget viverra lorem facilisis. Praesent a lectus ac ipsum tincidunt posuere vitae non risus. In eu feugiat massa. Sed eu est non velit facilisis facilisis vitae eget ante. Nunc ut malesuada erat. Nullam sagittis bibendum porta. Maecenas vitae interdum sapien, ut aliquet risus. Donec aliquet, turpis finibus aliquet bibendum, tellus dui porttitor quam, quis pellentesque tellus libero non urna. Vestibulum maximus pharetra congue. Suspendisse aliquam congue quam, sed bibendum turpis. Aliquam eu enim ligula. Nam vel magna ut urna cursus sagittis. Suspendisse a nisi ac justo ornare tempor vel eu eros.",
-            enumValues: {
-                private: "Private",
-                public: "Public"
-            }
+      }),
+  
+      hasSizes: buildProperty({
+        dataType: "boolean",
+        name: "Has Sizes",
+        description: "Does this product come in different sizes?",
+      }),
+  
+      quantity: buildProperty(({ values }) => ({
+        dataType: "number",
+        name: "Quantity",
+        description: "Total quantity available for this product.",
+        disabled: values.hasSizes && {
+          clearOnDisabled: true,
+          disabledMessage: "Quantity is not applicable for products with sizes.",
         },
-        published: ({ values }) => buildProperty({
-            name: "Published",
-            dataType: "boolean",
-            columnWidth: 100,
-            disabled: (
-                    values.status === "public"
-                        ? false
-                    : {
-                        clearOnDisabled: true,
-                        disabledMessage: "Status must be public in order to enable this the published flag"
-                    }
-            )
-        }),
-        related_products: {
-            dataType: "array",
-            name: "Related products",
-            description: "Reference to self",
-            of: {
-                dataType: "reference",
-                path: "products"
-            }
+        validation: { required: !values.hasSizes, min: 0 },
+      })),
+  
+      sizes: buildProperty(({ values }) => ({
+        dataType: "map",
+        name: "Sizes",
+        description: "Quantities for each size.",
+        properties: {
+          S: {
+            dataType: "number",
+            name: "Small",
+            validation: { required: false, min: 0 },
+          },
+          M: {
+            dataType: "number",
+            name: "Medium",
+            validation: { required: false, min: 0 },
+          },
+          L: {
+            dataType: "number",
+            name: "Large",
+            validation: { required: false, min: 0 },
+          },
         },
-        main_image: buildProperty({ // The `buildProperty` method is a utility function used for type checking
-            name: "Image",
-            dataType: "string",
-            storage: {
-                storagePath: "images",
-                acceptedFiles: ["image/*"]
-            }
-        }),
-        tags: {
-            name: "Tags",
-            description: "Example of generic array",
-            validation: { required: true },
-            dataType: "array",
-            of: {
-                dataType: "string"
-            }
+        disabled: !values.hasSizes && {
+          clearOnDisabled: true,
+          disabledMessage: "Sizes are only available if 'Has Sizes' is selected.",
+          hidden: true,
         },
-        description: {
-            name: "Description",
-            description: "This is the description of the product",
-            longDescription: "Example of a long description hidden under a tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis bibendum turpis. Sed scelerisque ligula nec nisi pellentesque, eget viverra lorem facilisis. Praesent a lectus ac ipsum tincidunt posuere vitae non risus. In eu feugiat massa. Sed eu est non velit facilisis facilisis vitae eget ante. Nunc ut malesuada erat. Nullam sagittis bibendum porta. Maecenas vitae interdum sapien, ut aliquet risus. Donec aliquet, turpis finibus aliquet bibendum, tellus dui porttitor quam, quis pellentesque tellus libero non urna. Vestibulum maximus pharetra congue. Suspendisse aliquam congue quam, sed bibendum turpis. Aliquam eu enim ligula. Nam vel magna ut urna cursus sagittis. Suspendisse a nisi ac justo ornare tempor vel eu eros.",
-            dataType: "string",
-            columnWidth: 300
-        },
-        categories: {
-            name: "Categories",
-            validation: { required: true },
-            dataType: "array",
-            of: {
-                dataType: "string",
-                enumValues: {
-                    electronics: "Electronics",
-                    books: "Books",
-                    furniture: "Furniture",
-                    clothing: "Clothing",
-                    food: "Food"
-                }
-            }
-        },
-        publisher: {
-            name: "Publisher",
-            description: "This is an example of a map property",
-            dataType: "map",
-            properties: {
-                name: {
-                    name: "Name",
-                    dataType: "string"
-                },
-                external_id: {
-                    name: "External id",
-                    dataType: "string"
-                }
-            }
-        },
-        expires_on: {
-            name: "Expires on",
-            dataType: "date"
-        }
-    }
-});
+  
+        validation: values.hasSizes ? { required: true } : undefined,
+      })),
+    },
+  });
 
 export default function CMS() {
 
@@ -199,7 +146,7 @@ export default function CMS() {
         name={"My Online Shop"}
         basePath={"/cms"}
         authentication={myAuthenticator}
-        collections={[productsCollection]}
+        collections={[ProductsCollection]}
         firebaseConfig={firebaseConfig}
     />;
 }
