ff260af2b2859caf5ae598f11c971a45e5679533
Usmankh4 <usmankhanx3@hotmail.com>
2023-12-20 21:24:06 -0500

    Added redux

---
+++ src/app/redux/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import shopperReducer from "./shopperSlice";
+
+export const store = configureStore({
+
+    reducer:{
+        shopper: shopperReducer, 
+    },
+});
