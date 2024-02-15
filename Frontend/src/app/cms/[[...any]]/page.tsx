2c92c214c7f77a87f76bed1388c607ef3e0ef1c9
Usmankh4 <usmankhanx3@hotmail.com>
2024-02-14 20:17:55 -0500

    Added the correct code for brand.js

--- Frontend/src/app/cms/[[...any]]/page.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const CMS = dynamic(async () => await import('../CMS'), {
- ssr: false,
-})
-export default function Home() {
-    return <CMS/>;
-}
--- Frontend/src/app/products/[brand]/page.js
+++ Frontend/src/app/products/[brand]/page.js
@@ -5,54 +5,27 @@ import Header from '@/components/header';
 import Footer from '@/components/footer';
 import phonesData from '../../phones.json';
 import Link from 'next/link';
-import db,{storage} from '@/firebase';
-import { useEffect } from 'react';
-import { doc, getDoc } from 'firebase/firestore';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
-import { collection, query, where, getDocs } from "firebase/firestore";
-import { useState } from 'react';
-import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 export default function BrandPage() {
   const { brand } = useParams(); // Retrieve the brand parameter
-
-  const [products, setProducts] = useState([]);
-// Outside of your component
-useEffect(() => {
-  const fetchProducts = async () => {
+  const [phones, setPhones] = useState([]); // State for storing the fetched phones data
+console.log(brand);
+  // Fetch phones data from your backend on component mount
+  useEffect(() => {
+    const fetchPhones = async () => {
     try {
-      const productsCollection = collection(db, "Products");
-      console.log(productsCollection);
-      console.log(brand);
-      const productsCollectionQuery = query(productsCollection, where("brand", "==", brand));
-      
-      console.log(productsCollectionQuery);
-      const querySnapshot = await getDocs(productsCollectionQuery);
-      const productsDataPromises = querySnapshot.docs.map(async (doc) => {
-        const data = doc.data();
-        try {
-          const imageUrl = await getDownloadURL(ref(storage, data.mainImage));
-          console.log(imageUrl); // Ensure URL is correct
-          return { ...data, mainImage: imageUrl };
-        } catch (error) {
-          console.error("Error getting image URL: ", error);
-          // Handle or return error appropriately
-          return { ...data, mainImage: null }; // or however you want to handle the error
-        }
-      });
-      const productsData = await Promise.all(productsDataPromises);
-      setProducts(productsData);
-      console.log(productsData);
+    const response = await axios.get(`http://localhost:8000/myapp/api/products/?brand=${brand}`);
+    console.log(response.data);
+    setPhones(response.data);
     } catch (error) {
-      console.error("Error fetching products: ", error);
-      // Handle error appropriately
+    console.error('Error fetching phones:', error);
     }
-  };
-
-  fetchProducts();
-}, [brand]); // Ensure this is the only dependency unless others are needed
-
-  
-
+    };
+    
+    fetchPhones();
+    }, [brand]);
 
   return (
     <div>
@@ -60,23 +33,22 @@ useEffect(() => {
         <Header />
         <div className="pageAfterHeader">
           <div className="PhoneTitle"> 
-            <h2>{brand.toUpperCase()}</h2>
+            <h2>{brand.toUpperCase()} PHONES</h2>
             <div className="PhoneWrapper">
               <div className="PhoneLayout">
-              {products.map((phone) => (
-  <div className="PhoneCard" key={phone.name}>
-    <h4>{phone.name}</h4>
-    <Link href={`/products/${phone.brand}/${phone.name}`}>
-     
-        <Image className="iPhoneImage" src={phone.mainImage} width={200} height={150}  alt={phone.name} />
-      
-    </Link>
-    <div className="PhonePrice">
-      <h4> on sale for ${phone.price}</h4>
-    </div>
-  </div>
-))}
-
+                {phones.map((phone) => (
+                  <div className="PhoneCard" key={phone.name}>
+                    <h4>{phone.name}</h4>
+                    <Link href={`/products/${brand}/${phone.name}`}>
+                      <div className="PhoneImage">
+                        <img src={phone.thumbnail} alt={phone.name} />
+                      </div>
+                    </Link>
+                    <div className="PhonePrice">
+                      <h4> on sale for ${phone.price}</h4>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
