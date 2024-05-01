ed3c88a61a28f1d501cb68cd2d8da5f4e51a61f4
Syed-Ahmed02 <55893792+Syed-Ahmed02@users.noreply.github.com>
2024-04-30 21:44:50 -0400

    added new event subscriptions to stripe webhook

--- Frontend/src/app/api/webhook/route.js
+++ Frontend/src/app/api/webhook/route.js
@@ -9,16 +9,66 @@ export async function POST(req ,res) {
     const sig = req.headers.get("Stripe-Signature")
     const dateTime = new Date(response?.created * 1000).toLocaleDateString()
     const timeString = new Date(response?.created * 1000).toLocaleDateString()
-
-    try {
-        let event = stripe.webhooks.constructEvent(
-            payload,
-            sig,
-            process.env.STRIPE_WEBHOOK_SECRET,
-        )
-        console.log("event", event.type)
-        return NextResponse.json({ status: "Success", event: event.type })
-    } catch (error) {
-        return NextResponse.json({ status: "Failed", error })
+    const relevantEvents = new Set([
+        'product.created',
+        'product.updated',
+        'price.created',
+        'price.updated',
+        'checkout.session.completed',
+        'customer.subscription.created',
+        'customer.subscription.updated',
+        'customer.subscription.deleted',
+      ]);
+      
+      try {
+        if (!sig || !webhookSecret) return;
+        event = stripeAdmin.webhooks.constructEvent(body, sig, webhookSecret);
+      } catch (error) {
+        return Response.json(`Webhook Error: ${(error).message}`, { status: 400 });
+      }
+    
+      if (relevantEvents.has(event.type)) {
+        try {
+          switch (event.type) {
+            case 'product.created':
+            case 'product.updated':
+              await upsertProduct(event.data.object );
+              break;
+            case 'price.created':
+            case 'price.updated':
+              await upsertPrice(event.data.object );
+              break;
+            case 'customer.subscription.created':
+            case 'customer.subscription.updated':
+            case 'customer.subscription.deleted':
+              const subscription = event.data.object ;
+              await upsertUserSubscription({
+                subscriptionId: subscription.id,
+                customerId: subscription.customer,
+                isCreateAction: false,
+              });
+              break;
+            case 'checkout.session.completed':
+              const checkoutSession = event.data.object;
+    
+              if (checkoutSession.mode === 'subscription') {
+                const subscriptionId = checkoutSession.subscription;
+                await upsertUserSubscription({
+                  subscriptionId: subscriptionId ,
+                  customerId: checkoutSession.customer ,
+                  isCreateAction: true,
+                });
+              }
+              break;
+            default:
+              throw new Error('Unhandled relevant event!');
+          }
+        } catch (error) {
+          console.error(error);
+          return Response.json('Webhook handler failed. View your nextjs function logs.', {
+            status: 400,
+          });
+        }
+      }
+      return Response.json({ received: true });
     }
-}
